bddf6964a781e9e5ed0b047c5b7a90b527b127d7
Vikas Varak <vvarak@deloitte.com>
2025-09-04 07:46:41 +0000

    commited

--- webapp/js/MassUploadParse.js
+++ webapp/js/MassUploadParse.js
@@ -2,8 +2,6 @@ sap.ui.define(["sap/m/MessageBox","uploadexcel/js/Xlsxfull"], function (MessageB
 	return {
 		getExcelData: function (file) {
 			return new Promise(function (resolve, reject) {
-				// Create a File Reader object (changed)
-				// created feature branch (changed)
 				var reader = new FileReader();
 				var excelData = {};
 				reader.onload = function (e) {
